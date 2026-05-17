bbd4de320254bc986ce243ea853848fc01bb2c84
PrabashSapkota <154650145+PrabashSapkota@users.noreply.github.com>
2026-05-17 22:54:31 +0545

    Delete Users/prabin/Documents/stream-refresher directory

--- Users/prabin/Documents/stream-refresher/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    iframeUrl: 'YOUR_IFRAME_URL_HERE',
-    port: 3000,
-    refreshIntervalMs: 60000,
-    tokenRefreshThresholdMs: 30000,
-    requestTimeout: 10000,
-    userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
-    referer: 'https://vishnu.modifiles.fans/'
-};
